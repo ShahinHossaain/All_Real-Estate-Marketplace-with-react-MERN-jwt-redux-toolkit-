d9faa19a51c34efd5ddbb898946c8fa3a8446b6c
ShahinHossaain <mdshahinhossaain@gmail.com>
2024-06-04 07:42:00 +0600

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
 
 const SignIn = () => {
     const [formData, setFormData] = useState({})
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false)
+    const { loading, error } = useSelector((state) => state.user)
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const handleChange = (e) => {
         setFormData({
@@ -16,25 +18,27 @@ const SignIn = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setLoading(true);
-        const res = await fetch('/api/auth/signin',
-            {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json',
-                },
-                body: JSON.stringify(formData)
+        try {
+            dispatch(signInStart());
+            const res = await fetch('/api/auth/signin',
+                {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json',
+                    },
+                    body: JSON.stringify(formData)
+                }
+            )
+            const data = await res.json()
+            if (data.success === false) {
+                dispatch(signInFailure(data.message))
+                return;
             }
-        )
-        const data = await res.json()
-        if (data.success === false) {
-            setError(data.message)
-            setLoading(false);
-            return;
+            dispatch(signInSuccess(data))
+            navigate('/')
+        } catch (error) {
+            dispatch(signInFailure(error.message))
         }
-        setLoading(false)
-        setError(null)
-        navigate('/')
     }
     return (
         <div className='p-3 max-w-lg mx-auto'>
