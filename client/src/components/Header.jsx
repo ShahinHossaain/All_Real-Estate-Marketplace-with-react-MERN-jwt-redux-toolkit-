7dc460cd516251b08b685292e26799cf909ee57f
ShahinHossaain <mdshahinhossaain@gmail.com>
2024-06-10 14:41:47 +0600

    complete profile page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ import { useSelector } from 'react-redux'
 const Header = () => {
 
     const { currentUser } = useSelector(state => state.user)
-    console.log('sata', currentUser)
+
 
     return (
         <header className='bg-slate-200 shadow-md'>
