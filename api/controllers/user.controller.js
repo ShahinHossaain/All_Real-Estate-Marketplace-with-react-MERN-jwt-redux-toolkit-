5efc956c32bc71ed01f8d80e0756654adb304253
ShahinHossaain <mdshahinhossaain@gmail.com>
2025-05-06 11:13:04 +0600

    jwt clear functionality update

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -55,7 +55,12 @@ export const deleteUser = async (req, res, next) => {
 
 export const signOut = async (req, res, next) => {
     try {
-        res.clearCookie('access_token');
+        res.clearCookie('access_token', {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'None',
+        });
+
         res.status(200).json('User has been logged out!');
     } catch (error) {
         next(error);
