35597c85bc473a5d4a035383de2b7d2e11918241
ShahinHossaain <mdshahinhossaain@gmail.com>
2025-05-06 09:15:42 +0600

    update

--- api/index.js
+++ api/index.js
@@ -23,8 +23,10 @@ mongoose.connect(process.env.MONGO)
 
 const app = express()
 app.use(cors({
-    origin: process.env.CLIENT_URL
+    origin: process.env.CLIENT_URL,
+    credentials: true
 }));
+
 
 app.use(express.json());
 app.use(cookieParser());
